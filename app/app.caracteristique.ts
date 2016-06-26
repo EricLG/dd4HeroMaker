f32c3b2c3185df71adf9bc7fbaa783618ac143d3
Pierre <pfremaux@gmail.com>
2016-06-26 23:11:12 +0200

    Inc/Decrementation des caracteristiques

--- app/app.caracteristique.ts
+++ app/app.caracteristique.ts
@@ -3,28 +3,51 @@ import { Component, Input, Output } from '@angular/core';
 @Component({
   selector: 'caracteristique',
   template:`<div class="row">
-        <div class="col-1">
-           &nbsp;
-        </div>
-		<div class="col-4">
-		    <div class="black-array">
-                    <div class="col-2"><input class="main-input-square-2" maxlength="3"></div>
-                    <div class="col-2">&nbsp;</div>
+        
+		<div class="col-3">
+		    <div class="dd4-black-array">
                     <div class="col-6">
-                       
                         <div class="centered">
                             <div class="white-text">{{bigTitle}}</div>
                             <div class="white-label">{{title}}</div>
                         </div>
                     </div>
+                    <input class="main-input-square-2" maxlength="3">
+                    <input type="button" class="main-input-circle black-button" value="-" (click)="clickDec()"/>
 			</div>
 		</div>
 		<div class="col-1">
-           &nbsp;
+            <div class="col-4 dd4-white-array">
+                <input type="button" class="main-input-circle white-button" value="+" (click)="clickInc()"/>
+            </div>
         </div>
-        <div class="col-3">
+        <div class="col-1 ">
+            <div class="col-4">
+               <div class="dd4-black-array">
+                       <div class="col-6 centered">
+                            <div class="white-text">&nbsp;</div>
+                            <div class="white-label">&nbsp;</div>
+                        </div>
+                        <div class="col-6">
+                            <input class="main-input-square-3" maxlength="3" value="{{character[bigTitle]}}">
+                        </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-1">
+             <div class="dd4-white-array">
+             =
+             </div>
+        </div>
+        <div class="col-2">
+            <div class="col-4 dd4-white-array">
+                <input class="main-input-square-3" maxlength="3">
+            </div>
+        </div>
+        <div class="col-1">
           <div class="col-4">
-           <div class="black-array">
+           <div class="dd4-black-array">
                    <div class="col-6 centered">
                         <div class="white-text">&nbsp;</div>
                         <div class="white-label">&nbsp;</div>
@@ -35,10 +58,9 @@ import { Component, Input, Output } from '@angular/core';
 			</div>
             </div>
         </div>
-        
-        <div class="col-3">
+        <div class="col-1">
           <div class="col-4">
-           <div class="black-array">
+           <div class="dd4-black-array">
                    <div class="col-6 centered">
                         <div class="white-text">&nbsp;</div>
                         <div class="white-label">&nbsp;</div>
@@ -54,9 +76,39 @@ import { Component, Input, Output } from '@angular/core';
 export class Caracteristique { 
     @Input() title = 't';
     @Input() bigTitle = 't';
+    @Input() character;
     
+    canInc():Boolean {
+        if (this.character.stockPoints > 0) {
+            return true;
+        }
+        return false;
+    }
+    canDec():Boolean {
+        if (this.caract() > 0) {
+            return true;
+        }
+        return false;
+    }
+    inc() {
+        this.character[this.bigTitle]++;
+    }
+    dec() {
+        this.character[this.bigTitle]++;
+    }
+    clickInc() {
+        if (this.canInc()) {
+            this.inc();
+            this.character.stockPoints--;
+        }
+    }
+    clickDec() {
+        if (this.canDec()) {
+            this.dec();
+            this.character.stockPoints++;
+        }
+    }
     constructor() {
         console.log('constructor '+this.title);
     }
-    
 }
--- app/app.components.ts
+++ app/app.components.ts
@@ -3,9 +3,17 @@ import { Caracteristique } from './app.caracteristique';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>My First Angular 2 App</h1>
-  <caracteristique [title]="'gfdgd'" [bigTitle]="'AAA'"></caracteristique>
+  template: `<h1>Creation du héros pas-à-pas</h1>
+  <p>Points en reserve {{datas.stockPoints}}</p>
+  <caracteristique [title]="'Force'" [bigTitle]="'FOR'" [character]="datas"></caracteristique>
+  <caracteristique [title]="'Constitution'" [bigTitle]="'CON'" [character]="datas"></caracteristique>
+  <caracteristique [title]="'Dexterité'" [bigTitle]="'DEX'" [character]="datas"></caracteristique>
+  <caracteristique [title]="'Intelligence'" [bigTitle]="'INT'" [character]="datas"></caracteristique>
+  <caracteristique [title]="'Sagesse'" [bigTitle]="'SAG'" [character]="datas"></caracteristique>
+  <caracteristique [title]="'Charisme'" [bigTitle]="'CHA'" [character]="datas"></caracteristique>
   `,
    directives:[Caracteristique]
 })
-export class AppComponent { }
+export class AppComponent {
+    datas={'FOR':5, 'CON':'3', 'DEX':6, stockPoints:10}
+}
